4946f584b35893e3677e8ab11a760a61098c58cd
DeadlyAlien <nicog99@live.com>
2022-09-05 06:09:08 -0500

    Added ability to remove list items on click

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 
 var list = document.querySelector("ul");
 var listItems = document.querySelectorAll("li");
-
+var itemNum = 3
 
 
 
 // Function that runs on each button click //
 function newElement() {
+
+
     // variable that creates a new "li" element
+    //'.setAttribute' gives each element a class and assigns it a number so that we can remove it
     var node = document.createElement("li");
+    node.setAttribute("id", itemNum);
+    node.setAttribute("onclick", "removeMe(this.id)")
+    
+    itemNum += 1;
+
     // gets the '.value' from the form 'itemInput' and the textbox 'itemDesc'
     var itemName = document.forms["itemInput"]["itemDesc"].value;
+
     // if the textbox is blank, send an alert to the browser and do nothing
     if (itemName == ""){
         alert("Cannot Submit Blank Field");
@@ -20,8 +29,15 @@ function newElement() {
 
     // Append child element to the node variable which is a list item using the itemName
     node.appendChild(document.createTextNode(itemName));
+
     // Get the "ul" element and append node
     document.querySelector("ul").appendChild(node);
+
     // Reset the form after submission
     document.getElementById("mainForm").reset();
+};
+
+
+function removeMe (id) {
+    document.getElementById(id).remove();
 };
